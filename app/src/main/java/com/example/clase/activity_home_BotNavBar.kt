be14e964e9e0f146478cb1359c20b01788fc334c
Oscar Romero <00059916@uca.edu.sv>
2022-10-25 21:44:01 +0200

    Correccion de las dependecias requeridas para el bottomNavBar

--- app/src/main/java/com/example/clase/activity_home_BotNavBar.kt
+++ app/src/main/java/com/example/clase/activity_home_BotNavBar.kt
@@ -2,6 +2,7 @@ package com.example.clase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.clase.R
 
 class activity_home_BotNavBar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
