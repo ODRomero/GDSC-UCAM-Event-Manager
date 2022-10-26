2b1148ec65cd3377698488685f20f797ce8d2783
JesusGlez13 <jcgonzalez0@alu.ucam.edu>
2022-10-26 17:44:59 +0200

    Añadiendo funcionalidad bbdd (creación de la base de datos y unión con el front end)

--- app/src/main/java/com/example/clase/MainActivity.kt
+++ app/src/main/java/com/example/clase/MainActivity.kt
@@ -1,39 +1,73 @@
 package com.example.clase
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var txtUsu: EditText?=null
+    var txtPass: EditText?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btLogin: Button = findViewById(R.id.btLogin)
+        txtUsu=findViewById(R.id.txtUsu)
+        txtPass=findViewById(R.id.txtPass)
 
 
 
     }
     fun regBt(v : View){
-        val intent : Intent = Intent(this@MainActivity,    activity_home_BotNavBar::class.java)
+        val intent : Intent = Intent(this@MainActivity,    activity_register::class.java)
         startActivity(intent)
     }
 
+    @SuppressLint("Range")
     fun loginBt(v : View){
-        println("he entrado")
-        val etPass : EditText = findViewById(R.id.etPassword)
-        val etCorreo : EditText = findViewById(R.id.etCorreo)
-        val pass : String = etPass.text.toString()
-        val correo : String = etCorreo.text.toString()
-        if(pass.equals("pass") && correo.equals("oscar")){
-            val intent : Intent = Intent(this@MainActivity, HomeActivity::class.java).apply {
-                putExtra("correo", correo)
+        var pass=txtPass?.text.toString()
+        var passAct:String?=""
+        var cont:Int=0
+
+        // Create or Instantiate the database
+        val dataBaseHelper = DataBaseHelper(applicationContext)
+
+        //Para recibir parámetros
+        //var intent:Intent=intent
+        //var userRecibed=intent.getStringExtra("UserName")
+        //var passRecibed=intent.getStringExtra("UserPass")
+
+        val db_writer = dataBaseHelper.writableDatabase
+        val selectQuery = "SELECT  * FROM Usuario WHERE password = ?"
+
+
+        db_writer.rawQuery(selectQuery, arrayOf(pass)).use { // .use requires API 16
+            if (it.moveToFirst()) {
+
+                passAct=it.getString(it.getColumnIndex("password"))
+                cont++
+
             }
-            startActivity(intent)
+
         }
+
+        if(cont>=1){
+            val home= Intent(this, activity_home_BotNavBar::class.java)
+            txtPass?.setText("")
+            txtUsu?.setText("")
+            startActivity(home)
+        }else{
+            Toast.makeText(this,"El usuario se debe loguear", Toast.LENGTH_LONG).show()
+        }
+        txtPass?.setText("")
+        txtUsu?.setText("")
     }
+
 }
