66181309ba9ef0249f11ffb33205ad3ea96519b5
Oscar Romero <00059916@uca.edu.sv>
2022-10-29 13:07:26 +0200

    Eliminacion del viejo home activity  e implementacion del frontend para la actividad detalles evento

---
+++ app/src/main/java/com/example/clase/activityDetallesEvento.kt
@@ -0,0 +1,11 @@
+package com.example.clase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activityDetallesEvento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalles_evento)
+    }
+}
