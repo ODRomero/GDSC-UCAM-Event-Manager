009ed4b19d2866fda36b4d436d4bed4089f18e57
JesusGlez13 <jcgonzalez0@alu.ucam.edu>
2022-10-31 16:10:08 +0100

    Implementando errores previos de boton "más info"

--- app/src/main/java/com/example/clase/DataBaseHelper.kt
+++ app/src/main/java/com/example/clase/DataBaseHelper.kt
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper
 class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         // If you change the database schema, you must increment the database version.
-        const val DATABASE_VERSION = 2
+        const val DATABASE_VERSION = 5
         const val DATABASE_NAME = "MySQLDataBase"
     }
     override fun onCreate(db: SQLiteDatabase) {
@@ -21,20 +21,18 @@ class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
             db.execSQL(tabla)
         }
         val tablas2 = arrayOf("insert into Ubicacion (direccion, aforo, nombre) values ('Ctra. Nora Guadalupe, 3A, 30107 Murcia', '50 Personas', 'UCAM HiTech')",
-            "insert into Ubicacion (direccion, aforo, nombre) values ('Av. de los Jerónimos 135, 30107 Guadalupe de Maciascoque, Murcia','28 Personas', 'UCAM Pabellón 5 Sótano API 6')",
             "insert into Ubicacion (direccion, aforo, nombre) values ('Plataforma GDSC UCAM', 'Ilimitado', 'Online')",
+            "insert into Ubicacion (direccion, aforo, nombre) values ('Av. de los Jerónimos 135, 30107 Guadalupe de Maciascoque, Murcia','28 Personas', 'UCAM Pabellón 5 Sótano API 6')",
             "insert into Facilitador (nombre, bio, email, foto) values ('Oscar Romero', 'Team Lead del GDSC UCAM', 'oromero@gdsc_ucam.com', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/events/IMG_20220129_120113_494.jpg')",
             "insert into Facilitador (nombre, bio, email, foto) values('Jesus Gonzalez', 'Core Team Member del GDSC UCAM', 'jgonzalez@gdsc_ucam.com', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/events/ezgif.com-gif-maker%20%282%29.png')",
-            "insert into Facilitador (nombre, bio, email, foto) values('Jorge Arcas', 'Core Team Member del GDSC UCAM', 'jarcas@gdsc_ucam.com', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/events/ezgif.com-gif-maker%20%282%29.png')",
+            "insert into Facilitador (nombre, bio, email, foto) values('Jorge Arcas', 'Core Team Member del GDSC UCAM', 'jarcas@gdsc_ucam.com', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/jorge_arcas.jfif')",
             "insert into Evento (nombre, fecha, descripcion, foto, FID, UBID) values('Presentacion GDSC UCAM', 'Oct 4, 2022', 'Presentaremos al equipo que conforma al equipo del GDSC UCAM y los tipo de eventos que queremos realizar este año', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_xy_center,h_650,q_auto:good,w_1200,x_w_mul_0.5,y_h_mul_0.38/v1/gcs/platform-data-dsc/event_banners/Event%20Promo%20-%20Presentacion_lJi3o2J.png', '1', '1' )",
-            "insert into Evento (nombre, fecha, descripcion, foto, FID, UBID) values('Taller Python Basico', 'Oct 15, 2022', 'Introduccion al lenguaje de progracion Python', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_xy_center,h_650,q_auto:good,w_1200,x_w_mul_0.5,y_h_mul_0.65/v1/gcs/platform-data-dsc/event_banners/promo%20taller%20basico%20de%20python.png', '2', '3')",
-            "insert into Evento (nombre, fecha, descripcion, foto, FID, UBID) values('Taller GitHub Basico', 'Oct 11, 2022', 'Introduccion al lenguaje de progracion Python', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_xy_center,h_650,q_auto:good,w_1200,x_w_mul_0.5,y_h_mul_0.55/v1/gcs/platform-data-dsc/event_banners/GitHub%20Basico%20Promo%20-%20Green.jpg', '3', '2')",
+            "insert into Evento (nombre, fecha, descripcion, foto, FID, UBID) values('Taller Python Basico', 'Oct 15, 2022', 'Introduccion al lenguaje de progracion Python', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_xy_center,h_650,q_auto:good,w_1200,x_w_mul_0.5,y_h_mul_0.65/v1/gcs/platform-data-dsc/event_banners/promo%20taller%20basico%20de%20python.png', '2', '2')",
+            "insert into Evento (nombre, fecha, descripcion, foto, FID, UBID) values('Taller GitHub Basico', 'Oct 11, 2022', 'Introduccion al control de versiones con Github', 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_xy_center,h_650,q_auto:good,w_1200,x_w_mul_0.5,y_h_mul_0.55/v1/gcs/platform-data-dsc/event_banners/GitHub%20Basico%20Promo%20-%20Green.jpg', '3', '3')",
             "insert into Usuario (email, nombre, password, telefono) values ('oscar@gdsc.com', 'Oscar Romero', 'a', '123456789')",
             "insert into UserEvent (UID, EID) values (1,1)",
             "insert into UserEvent (UID, EID) values (1,2)"
-
         )
-
         for (tabla2 in tablas2){
             db.execSQL(tabla2)
         }
